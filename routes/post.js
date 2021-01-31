a86bb3ca6cfb1bcca74e0967bda79a1b1ac1d0d2
MagsudMirzazada <magsud.mirzezade@gmail.com>
2021-02-01 00:48:21 +0400

    with validator

--- routes/post.js
+++ routes/post.js
@@ -3,12 +3,24 @@ const conn = require('../database');
 const express = require('express');
 const router = express.Router();
 
+// validator
+const {check, validationResult} = require('express-validator');
+
 
 router.get('/', (req, res) => {
     res.render('post');
 });
 
-router.post('/', (req, res) => {
+router.post('/', [
+    check('inputPAS')
+    .isInt().withMessage('Enter Integer')
+    .isLength({min:1, max: 3}).withMessage('Not proper number')
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
     if (req.body.inputFROM && req.body.inputTO && req.body.inputDEP && req.body.inputARR && req.body.inputAIR && req.body.inputPAS){
         let sql = "INSERT INTO FlightDB (from_city, to_city, departure_time, arrival_time, airplane_info, passengers_count) VALUES (?)";
         let values = [req.body.inputFROM, req.body.inputTO, req.body.inputDEP, req.body.inputARR, req.body.inputAIR, req.body.inputPAS];
